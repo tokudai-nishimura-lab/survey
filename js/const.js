f9b64fca2642dcab26cb0d50c196b4c0e09a534d
Kai1467 <84659368+Kai1467@users.noreply.github.com>
2025-01-29 15:38:46 +0900

    Add files via upload
    問題の出題順をランダムにする機能を追加、ページ数を追加
    Add feature to randomize question order and increase page count

--- js/const.js
+++ js/const.js
@@ -13,31 +13,53 @@
 const PostURL = "http://localhost:8080/save_data";
 
 /**
+ * MOS関連の音声リスト(テスト用)
+ *
+ * ※変更ポイント（使用する音声に変更して下さい）
+ */
+const MOStestaudioFiles = [
+  "../wav/_num/0.wav",
+  "../wav/_num/10.wav",
+  "../wav/_num/20.wav",
+];
+
+/**
  * MOS関連の音声リスト
  *
  * ※変更ポイント（使用する音声に変更して下さい）
  */
 const MOSaudioFiles = [
-  "../wav/_num/1.wav",
-  "../wav/_num/2.wav",
-  "../wav/_num/3.wav",
-  "../wav/_num/4.wav",
-  "../wav/_num/5.wav",
-  "../wav/_num/6.wav",
-  "../wav/_num/7.wav",
-  "../wav/_num/8.wav",
-  "../wav/_num/9.wav",
-  "../wav/_num/10.wav",
-  "../wav/_num/11.wav",
-  "../wav/_num/12.wav",
-  "../wav/_num/13.wav",
-  "../wav/_num/14.wav",
-  "../wav/_num/15.wav",
-  "../wav/_num/16.wav",
-  "../wav/_num/17.wav",
-  "../wav/_num/18.wav",
-  "../wav/_num/19.wav",
-  "../wav/_num/20.wav",
+  ["../wav/_num/1.wav", 0,],
+  ["../wav/_num/2.wav", 1,],
+  ["../wav/_num/3.wav", 2,],
+  ["../wav/_num/4.wav", 3],
+  ["../wav/_num/5.wav", 4],
+  ["../wav/_num/6.wav", 5],
+  ["../wav/_num/7.wav", 6],
+  ["../wav/_num/8.wav", 7],
+  ["../wav/_num/9.wav", 8],
+  ["../wav/_num/10.wav", 9],
+  ["../wav/_num/11.wav", 10],
+  ["../wav/_num/12.wav", 11],
+  ["../wav/_num/13.wav", 12],
+  ["../wav/_num/14.wav", 1,],
+  ["../wav/_num/15.wav", 1,],
+  ["../wav/_num/16.wav", 15],
+  ["../wav/_num/17.wav", 16],
+  ["../wav/_num/18.wav", 17],
+  ["../wav/_num/19.wav", 18],
+  ["../wav/_num/20.wav", 19],
+];
+
+/**
+ * DMOS関連の音声リスト（テスト用）
+ *
+ * ※変更ポイント（使用する音声に変更して下さい）
+ */
+const DMOStestaudioFiles = [
+  ["../wav/_num/0.wav", "../wav/_num/1.wav",],
+  ["../wav/_num/0.wav", "../wav/_num/2.wav",],
+  ["../wav/_num/0.wav", "../wav/_num/3.wav",],
 ];
 
 /**
@@ -46,17 +68,18 @@ const MOSaudioFiles = [
  * ※変更ポイント（使用する音声に変更して下さい）
  */
 const DMOSaudioFiles = [
-  ["../wav/_num/1.wav", "../wav/_num/2.wav"],
-  ["../wav/_num/3.wav", "../wav/_num/4.wav"],
-  ["../wav/_num/5.wav", "../wav/_num/6.wav"],
-  ["../wav/_num/7.wav", "../wav/_num/8.wav"],
-  ["../wav/_num/9.wav", "../wav/_num/10.wav"],
-  ["../wav/_num/11.wav", "../wav/_num/12.wav"],
-  ["../wav/_num/13.wav", "../wav/_num/14.wav"],
-  ["../wav/_num/15.wav", "../wav/_num/16.wav"],
-  ["../wav/_num/17.wav", "../wav/_num/18.wav"],
-  ["../wav/_num/19.wav", "../wav/_num/20.wav"],
+  ["../wav/_num/1.wav", "../wav/_num/2.wav", 0],
+  ["../wav/_num/3.wav", "../wav/_num/4.wav", 1],
+  ["../wav/_num/5.wav", "../wav/_num/6.wav", 2],
+  ["../wav/_num/7.wav", "../wav/_num/8.wav", 3],
+  ["../wav/_num/9.wav", "../wav/_num/10.wav", 4],
+  ["../wav/_num/11.wav", "../wav/_num/12.wav", 5],
+  ["../wav/_num/13.wav", "../wav/_num/14.wav", 6],
+  ["../wav/_num/15.wav", "../wav/_num/16.wav", 7],
+  ["../wav/_num/17.wav", "../wav/_num/18.wav", 8],
+  ["../wav/_num/19.wav", "../wav/_num/20.wav", 9],
 ];
+
 
 /**
  * HTMLのファイルリスト
@@ -75,16 +98,30 @@ let HTML = [
   {
     category: "form",
     html: "form.html",
-    total: 3,
-    perpage: 3,
+    total: 7,
+    perpage: 7,
     action: "ValidFormInfo",
+  },
+  {
+    category: "mos_pra",
+    html: "mos_pra.html",
+    total: MOStestaudioFiles.length,
+    perpage: 5,
+    action: "ValidSkipInfo",
   },
   {
     category: "mos",
     html: "mos.html",
     total: MOSaudioFiles.length,
-    perpage: 10,
+    perpage: 5,
     action: "ValidMOSInfo",
+  },
+  {
+    category: "dmos_pra",
+    html: "dmos_pra.html",
+    total: DMOStestaudioFiles.length,
+    perpage: 5,
+    action: "ValidSkipInfo",
   },
   {
     category: "dmos",
